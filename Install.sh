42a54d395b7bdfac607ee5fd8a70796b18b13cd3
Veronica Guerrini <44610993+veronicaguerrini@users.noreply.github.com>
2023-03-31 23:04:11 +0200

    Upload files

---
+++ Install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+INSTALL_PREPROCESSING=1
+SHORT=1
+
+#To install the following tools:
+#seqtk
+#gsufsort
+#BCR
+
+if [ $INSTALL_PREPROCESSING -eq 1 ]
+then
+	mkdir Preprocessing
+	cd ./Preprocessing
+
+	echo -e "\n****Dowloading seqtk...\n"
+	git clone https://github.com/lh3/seqtk.git;
+	cd seqtk
+	echo -e "\n****Compiling seqtk...\n"
+	make
+	
+	cd ..
+	echo -e "\n****Dowloading gsufsort...\n"
+	git clone https://github.com/felipelouza/gsufsort
+	cd gsufsort
+	echo -e "\n****Compiling gsufsort...\n"
+	make TERMINATOR=0 DNA=1 
+	
+	cd ..
+	echo -e "\n****Dowloading BCR...\n"
+	git clone https://github.com/giovannarosone/BCR\_LCP\_GSA
+	cd BCR\_LCP\_GSA
+	cp ./../../BCR_Parameters.h Parameters.h
+	echo -e "\n****Compiling BCR...\n"
+	make
+	
+	cd ../..
+fi
+
+
+#Compile phyBWT
+# make clean 
+# if [ $SHORT -eq 1 ]
+# then
+	# echo -e "\n****Compiling phyBWT...\n"
+	# make
+# else
+	# echo -e "\n****Compiling phyBWT SHORT=0...\n"
+	# make SHORT=0
+# fi
+
+# echo -e "\nDone."
+
---
+++ Preprocessing.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+####################################################################
+####################################################################
+#Script for building the three data structures (ebwt, cda and lcp)
+#from a collection of fasta files
+####################################################################
+####################################################################
+
+SHORT=0
+
+#Fasta files path
+PathDataset=$1
+
+#Output files name
+fastaName=$2
+
+############################
+InfoFile=$fastaName".txt"
+FastaDataset=$fastaName".fasta"
+
+############################
+#Paths for installed tools
+############################
+pathseqtk="./Preprocessing/seqtk"
+pathBCR="./Preprocessing/BCR_LCP_GSA"
+pathgsufsort="./Preprocessing/gsufsort"
+############################
+############################
+
+> $InfoFile
+> $FastaDataset
+
+echo -e "\nComputing a single fasta file..."
+
+for file in $PathDataset/*
+do
+	NAME=$(basename "$file" .fasta)
+	echo $NAME
+	#Count string number
+	nReads=$(grep ">" $file | wc -l)
+	#Append F+RC
+	$pathseqtk/seqtk seq -U $file  >> $FastaDataset
+	$pathseqtk/seqtk seq -r -U $file >> $FastaDataset	
+	#Write fileInfo
+	nReads=$(($nReads*2))
+	echo -e $NAME"\t"$nReads >> $InfoFile
+done
+
+echo -e "\nComputing eBWT/DA/LCP..."
+
+if [ $SHORT -eq 1 ]
+then
+	#BCR for eBWT/LCP/DA
+	/usr/bin/time -v $pathBCR/BCR_LCP_GSA $FastaDataset $FastaDataset > "BCR_"$(basename "$FastaDataset" .fasta)".stdout" 2> "BCR_"$(basename "$FastaDataset" .fasta)".stderr"
+	rm *.len
+	rm *.info
+else
+	#gsufsort for eBWT/LCP/DA
+	/usr/bin/time -v $pathgsufsort/"gsufsort-64" $FastaDataset --bwt --lcp 4 --da 4 > "gsufsort_"$(basename "$FastaDataset" .fasta)".stdout" 2> "gsufsort_"$(basename "$FastaDataset" .fasta)".stderr"
+	mv $FastaDataset".bwt" $FastaDataset".ebwt"
+	mv $FastaDataset".4.da" $FastaDataset".da"
+	mv $FastaDataset".4.lcp" $FastaDataset".lcp"
+fi
+echo "Done."
+
+echo -e "\nComputing CDA..."
+./create_cda $FastaDataset $InfoFile
+echo "Done."
+####
+####
+
